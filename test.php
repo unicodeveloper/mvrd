3b5f464b5d32bbad677b7ebbf831c32f274ee07a
PROSPER OTEMUYIWA <prosperotemuyiwa@gmail.com>
2016-12-17 07:50:49 +0100

    Add comments for better illustration

--- test.php
+++ test.php
@@ -2,9 +2,12 @@
 
 require 'vendor/autoload.php';
 
+// Import Mvrd 
 use Unicodeveloper\Mvrd\Mvrd;
 
-
+// Instantiate Mvrd and pass a valid plate number
 $obj = new Mvrd('xxxxxx');
 
+// Call the getData method to return an array with the following details:
+// plateNumber, Owner Name, Color, Model, Chasis Number, Vehicle Status, License Issue Date and Expiry Date
 print_r($obj->getData()[0]);
